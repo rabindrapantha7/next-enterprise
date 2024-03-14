3fbe0c88bac980e6dc369b179f31088c20c1a989
Rabindra Pantha <rabindra@Rabindras-MacBook-Pro.local>
2024-03-14 20:35:40 +0545

    Add an item to the list

--- lp-items.tsx
+++ lp-items.tsx
@@ -413,4 +413,24 @@ export const LP_GRID_ITEMS = [
       </svg>
     ),
   },
+  {
+    title: "Mechanic",
+    description: "For automatic changelog generation.",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    ),
+  },
 ]
